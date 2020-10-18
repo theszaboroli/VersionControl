839dc787d973b549e48d22ee0323358c78fcfd24
Roland Szabo <theszaboroli@gmail.com>
2020-10-18 19:12:46 +0200

    8.feladat

--- 6. gyak/6. gyak/Form1.cs
+++ 6. gyak/6. gyak/Form1.cs
@@ -16,13 +16,34 @@ namespace _6.gyak
 {
     public partial class Form1 : Form
     { BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<String> Currencies = new BindingList<string>();
         public string rslt;
        
         public Form1()
         {
             InitializeComponent();
+
+            GetCurrencies();
             RefreshData();
 
+        }
+
+        private void GetCurrencies()
+        {
+            var mnbService2 = new MNBArfolyamServiceSoapClient();
+            var request = new GetCurrenciesRequestBody();
+            var response = mnbService2.GetCurrencies(request);
+            var result = response.GetCurrenciesResult;
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                for (int i = 0; i < element.ChildNodes.Count; i++)
+                {
+                    var childElement = (XmlElement)element.ChildNodes[i];
+                    Currencies.Add(childElement.InnerText);
+                }
+            }
         }
 
         private void RefreshData()
@@ -34,6 +55,7 @@ namespace _6.gyak
             GetXML();
 
             GetDiagram();
+            comboBox1.DataSource = Currencies;
             
         }
 
