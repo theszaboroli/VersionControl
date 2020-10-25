5d786ce83822fce44de832ab8f5edb02c023f571
Roland Szabo <theszaboroli@gmail.com>
2020-10-25 18:39:35 +0100

    3.

---
+++ week07/week07/Entites/BirthProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entites
+{
+    class BirthProbability
+    {
+        
+      
+        public int kor { get; set; }
+        public int gyermekek { get; set; }
+        public double p { get; set; }
+    }
+}
---
+++ week07/week07/Entites/DeathProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entites
+{
+    class DeathProbability
+    {
+       
+        public int nem { get; set; }
+        public Gender Gender { get; set; }
+        public int kor { get; set; }
+        public double p { get; set; }
+
+    }
+}
