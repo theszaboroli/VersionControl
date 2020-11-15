4dd5a5c96e752e27dd2ab1a8138c70576007db1f
Roland Szabo <theszaboroli@gmail.com>
2020-11-15 15:23:47 +0100

    5

---
+++ 8_gyak/8_gyak/Abstractions/Toy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_gyak.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        private void DrawImage(Graphics graphics)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract void DrawImage(Graphics g);
+
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
