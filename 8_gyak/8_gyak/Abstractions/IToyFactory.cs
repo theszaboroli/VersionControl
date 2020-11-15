7f73ca90e34248a3b9155ae1936b006f1fe74520
Roland Szabo <theszaboroli@gmail.com>
2020-11-15 15:35:55 +0100

    8

---
+++ 8_gyak/8_gyak/Abstractions/IToyFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_gyak.Abstractions
+{
+   public interface IToyFactory
+    Toy CreateNew();
+}
+}
