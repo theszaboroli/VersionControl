c09ee69f643281da5dfbba7e8d642392ced8cefe
Roland Szabo <theszaboroli@gmail.com>
2020-11-15 16:22:07 +0100

    14

---
+++ 8_gyak/8_gyak/Entities/Present.cs
@@ -0,0 +1,23 @@
+﻿using _8_gyak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_gyak.Entities
+{
+    class Present : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public S(Color SZALAribbon, Color box)
+        {
+            FillRectangle
+        }
+    }
+}
--- 8_gyak/8_gyak/Form1.Designer.cs
+++ 8_gyak/8_gyak/Form1.Designer.cs
@@ -36,11 +36,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
+            this.mainPanel.Controls.Add(this.button4);
             this.mainPanel.Controls.Add(this.button3);
             this.mainPanel.Controls.Add(this.label1);
             this.mainPanel.Controls.Add(this.button2);
@@ -101,6 +103,15 @@
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(473, 261);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Present";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -124,6 +135,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
