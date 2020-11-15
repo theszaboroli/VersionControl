7df6e7585ab601a62257d30e8b773d17d1b9ffb0
Roland Szabo <theszaboroli@gmail.com>
2020-11-15 15:26:13 +0100

    6

--- 8_gyak/8_gyak/Entities/Ball.cs
+++ 8_gyak/8_gyak/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _8_gyak.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,29 +9,13 @@ using System.Windows.Forms;
 
 namespace _8_gyak.Entities
 {
-   public class Ball : Label
-    {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+   public class Ball : Toy
+    { 
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
-        {
-            Left += 1;
-        }
+     
     }
 }
