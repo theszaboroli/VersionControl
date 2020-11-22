559f5cc3d8f6b357a6431e512177f2c8a3c0dcd7
Roland Szabo <theszaboroli@gmail.com>
2020-11-22 12:20:21 +0100

    8

--- week_09/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week_09/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
@@ -16,6 +18,28 @@ namespace UnitTestExample.Test
     TestCase("irf@uni-corvinus.hu", true)
 ]
         public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+
+        [
+   Test,
+    TestCase("abcdABCD", false),
+    TestCase("ABCD1234", false),
+    TestCase("abcd1234", false),
+    TestCase("irf@uni-corvinus.hu", true)
+            
+]
+        public void TestValidatePass(string email, bool expectedResult)
         {
             // Arrange
             var accountController = new AccountController();
