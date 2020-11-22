04a1caf5c635a71bc1a54261cd7b2fb850e6c23b
Roland Szabo <theszaboroli@gmail.com>
2020-11-22 12:43:05 +0100

    11_final

--- week_09/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week_09/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,11 +1,14 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Abstractions;
 using UnitTestExample.Controllers;
+using UnitTestExample.Entities;
 
 namespace UnitTestExample.Test
 {
@@ -95,6 +98,40 @@ namespace UnitTestExample.Test
             catch (Exception ex)
             {
                 Assert.IsInstanceOf<ValidationException>(ex);
+            }
+
+            // Assert
+        }
+
+
+        //11//
+
+
+
+
+            [
+    Test,
+    TestCase("irf@uni-corvinus.hu", "Abcd1234")
+]
+        public void TestRegisterApplicationException(string newEmail, string newPassword)
+        {
+            // Arrange
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Throws<ApplicationException>();
+            var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(newEmail, newPassword);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ApplicationException>(ex);
             }
 
             // Assert
