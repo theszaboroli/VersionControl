b7c05f1cc3e5ee16075cc8e454bcd4f42d3c09b7
Roland Szabo <theszaboroli@gmail.com>
2020-11-28 18:43:30 +0100

    5

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -29,6 +29,26 @@ namespace week10
             gc.AddPlayer();
             gc.Start(true);
 
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
