f601aa6665f3890605877b1a0fff6137b8762aa5
Roland Szabo <theszaboroli@gmail.com>
2020-11-28 18:53:31 +0100

    FINAL

--- week10/week10/Form1.Designer.cs
+++ week10/week10/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(162, 402);
+            this.button1.Location = new System.Drawing.Point(191, 402);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
