7a4286a1febc5751f77735ee3abf2b67452e4f86
Roland Szabo <theszaboroli@gmail.com>
2020-11-28 18:46:00 +0100

    6

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -49,6 +49,22 @@ namespace week10
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
